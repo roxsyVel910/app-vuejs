2cececba5a3bed4b20c403a922b229851f819b53
roxsyVq <roxsy.dj@gmail.com>
2024-07-11 09:59:40 -0500

    creacion de  components

---
+++ src/components/PostButton.vue
@@ -0,0 +1,21 @@
+<Template>
+    <button class="post-btn">ACCEDER</button>
+</Template>
+
+<Script lang="ts" setup>
+</Script>
+<style scoped>
+.post-btn {
+    
+    padding: 10px 20px;
+    border-radius: 5px;
+    color: #fff;
+    background-color: #007bff;
+    border: none;
+}
+.post-btn:hover {
+    
+    background-color: #0069d9;
+    color: #fff;
+}
+</style>
