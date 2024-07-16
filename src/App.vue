0b7c5890a448184a0b9e9442135f3808ad9e86d6
roxsyVq <roxsy.dj@gmail.com>
2024-07-16 08:40:38 -0500

    agregando lista

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <Count></Count>
+  <ListaDeTareas> </ListaDeTareas>
 </template>
 
 <script setup>
 import Count from './components/Contadorr.vue';
-
+import ListaDeTareas from './components/ListaDeTareas.vue'
 </script>
 
 <style scoped>
