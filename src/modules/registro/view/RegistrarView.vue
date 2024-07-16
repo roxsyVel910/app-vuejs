461d2cffc70e6b3d6b392560d30d4d77075a2e27
roxsyVq <roxsy.dj@gmail.com>
2024-07-16 14:59:58 -0500

    utilizando pinia

--- src/modules/registro/view/RegistrarView.vue
+++ src/modules/registro/view/RegistrarView.vue
@@ -4,12 +4,12 @@
     <Form :validation-schema="schema" @submit="onSubmit">
       <div class="form">
         <label for=" nombre">Nombre:</label>
-        <Field type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" />
+        <Field v-model="nombre" type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre" />
         <ErrorMessage name="nombre"></ErrorMessage>
     </div>
     <div class="form">
         <label for="correo">Correo:</label>
-        <Field type="email" name="email" placeholder=" ingrese su email" />
+        <Field v-model="email" type="email" name="email" placeholder=" ingrese su email" />
         <ErrorMessage name="email"></ErrorMessage>
       </div>
       <div class="form">
@@ -21,8 +21,15 @@
 <script setup>
 import {Form, Field, ErrorMessage}  from 'vee-validate';
 import {schema} from '../schemas/validationSchemas';
+import { useRegistrarStore} from '../stores/registrarStore'
+import {ref} from 'vue';
+
+const registrarStore = useRegistrarStore();
+
+const nombre = ref('');
+const email= ref('');
 const onSubmit = () => {
-    console.log(`se ha enviado el formulario`);
+    registrarStore.guardarRegistro(nombre.value, email.value);
 }
 </script>
 
