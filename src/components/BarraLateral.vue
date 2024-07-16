7ddcd70ba31ec9166ab723035070e0957f7eeb19
roxsyVq <roxsy.dj@gmail.com>
2024-07-16 15:23:17 -0500

    create store de pinia para almacenar datos de registro

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sidebar">
-    <h2>Barra lateral</h2>
+    <h2>Bienvenido</h2>
+    <h3>Nombre: {{ nombre }} </h3>
+    <h3>Email: {{ email }} </h3>
     <ul>
       <li><router-link to="/contador">Contador</router-link></li>
       <li><router-link to="/lista-de-tareas">Lista De Tareas</router-link></li>
@@ -8,7 +10,23 @@
     </ul>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
+import {ref, watch} from 'vue';
+
+const registrarStore = useRegistrarStore();
+const nombre = ref(registrarStore.nombre.value);
+const email = ref(registrarStore.email.value);
+
+watch(() => registrarStore.nombre, (newValue) => {
+    nombre.value = newValue;
+});
+
+watch(() => registrarStore.email, (newValue) => {
+    email.value = newValue;
+})
+
+</script>
 <style>
 .sidebar {
   width: 200px;
